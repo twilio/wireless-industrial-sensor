b6eab60c31ee98597891ca16d54f3b73d1c19f1c
Jon Davis <jonerikdavis@gmail.com>
2017-08-24 17:03:16 -0700

    electron

--- electron/main.ino
+++ electron/main.ino
@@ -1,56 +1,53 @@
+#include <Si7020.h>
+#include <HX711ADC.h>
 #include "Si7020/Si7020.h"
-#include "ThingSpeak/ThingSpeak.h"
-#include "HX711ADC/HX711ADC.h"
-#include "cellular_hal.h"
 
-//We need to use the Twilio APN
+// We need to use the Twilio APN
 STARTUP(cellular_credentials_set("wireless.twilio.com", "", "", NULL));
 
-HX711ADC beans(C4, C5);
+HX711ADC cells(C4, C5);
 Si7020 sensor;
 
-TCPClient client;
-unsigned long myChannelNumber = 101;            //Place your Thingspeak Channel Number Here
-const char * myWriteAPIKey = "YourThingspeakAPIKey";
+// this value is obtained by calibrating the scale with known weights; 
+// https://learn.sparkfun.com/tutorials/load-cell-amplifier-hx711-breakout-hookup-guide
+float calibration_factor = 1500;
 
-float calibration_factor = 6350;            // this value is obtained by calibrating the scale with known weights; https://learn.sparkfun.com/tutorials/load-cell-amplifier-hx711-breakout-hookup-guide
+const char * id = "One";
+const char * name = "MyIndustrialSensor";
+const char * token = "2TZNkmwiQcy8E5Jl";
 
 void setup() {
+  // Initialize our temperature sensor
+  sensor.begin();
 
-    //Initialise our ThingSpeak channel
-    ThingSpeak.begin(client);
+  // Initilialize the HX711 ADC for our load cell
+  cells.set_scale(calibration_factor);
+  // Reset the scale to 0
+  cells.tare();
 
-    //Initialise our temperature sensor
-    sensor.begin();
-
-    //Initilialise the HX711 ADC for our load cell
-    beans.set_scale(calibration_factor);
-    beans.tare();                    // reset the scale to 0
-
+  Serial.begin(9600);
 }
 
 void loop() {
+  // Measure Weight average over 10 samples (in ozs)
+  int weight = cells.get_units(10);
 
-    // Measure Weight
-    int weight = beans.get_units(10);       //Average over 10 samples (in ozs)
+  // Measure Relative Humidity
+  double rh = sensor.getRH();
 
-    // Measure Relative Humidity
-    float rh = sensor.getRH();
+  // Measure Temperature + convert from C to F
+  double t = sensor.getTemp();
+  t = (t * 1.8) + 32;
 
-    // Measure Temperature
-    float t = sensor.getTemp();
-    t = (t * 1.8)+32;                       //Convert from C to F
+  // Send data to Particle server
+  // Using publish so listeners can respond accordingly
+  char data[200];
+  sprintf(data, "{\"id\":\"%s\", \"name\":\"%s\", \"weight\": %d, \"humidity\": %f, \"temperature\": %f, \"token\": \"%s\"}", id, name, weight, rh, t, token);
 
-    // Set our channel fields with our data
-    ThingSpeak.setField(1,weight);
-    ThingSpeak.setField(2,t);
-    ThingSpeak.setField(3,rh);
+  Particle.publish("Industrial Sensor Measurement", data, PRIVATE);
 
-    // Write the fields that we've set all at once.
-    ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-
-    beans.power_down();                 // put the ADC in sleep mode
-    delay(10000);                           // ThingSpeak will only accept updates every 15 seconds so make sure we wait at least this length of time - Note: Include the time taken to average the load cell values
-    beans.power_up();
+  // Put the ADC in sleep mode
+  cells.power_down();
+  delay(60000);
+  cells.power_up();
 }
-
