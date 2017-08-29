de9f86fa71dcd255f6cf077e39dae822b8f838b3
Jon Davis <jonerikdavis@gmail.com>
2017-08-29 14:28:44 -0700

    sensor readings by sync

--- angular/js/index.js
+++ angular/js/index.js
@@ -1,58 +1,75 @@
 'use strict';
 
-const $ = require("jquery");
-const angular = require("angular");
-const moment = require("moment");
-require("angular-route");
+const $ = require ('jquery');
+const angular = require ('angular');
+const moment = require ('moment');
+require ('angular-route');
 
-const MOMENT_FORMAT = "MMM DD YYYY @ hh:mm";
+const MOMENT_FORMAT = 'MMM DD YYYY @ hh:mm';
 
 // style sheets
-require("bootstrap-webpack");
-require("../scss/main.scss");
+require ('bootstrap-webpack');
+require ('../scss/main.scss');
 
 // index.html
-require("../index.html");
+require ('../index.html');
 
-const dashboardView = require("./dashboardView");
-const sensorListView = require("./sensorListView");
+const dashboardView = require ('./dashboardView');
+const sensorListView = require ('./sensorListView');
 
 var currentView;
 var $currentViewScope;
 
-var App = require("./app");
-window.app = new App({
+var App = require ('./app');
+window.app = new App ({
   refresh: function () {
-    $currentViewScope.$apply();
-  }
+    $currentViewScope.$apply ();
+  },
 });
 
 angular
-  .module("app", [
-    'ngRoute'
+  .module ('app', ['ngRoute'])
+  .controller ('DashboardViewCtrl', [
+    '$scope',
+    function ($scope) {
+      $currentViewScope = $scope;
+      currentView = dashboardView;
+      $.when (app.initialized).done (function () {
+        $scope.$evalAsync(function() {
+          dashboardView.init (app, $scope);
+        });
+      });
+    },
   ])
-  .controller('DashboardViewCtrl', ['$scope', function ($scope) {
-    $currentViewScope = $scope;
-    currentView = dashboardView;
-    $.when(app.initialized).done(function () {
-      dashboardView.init(app, $scope);
-    });
-  }])
-  .controller('SensorListViewCtrl', ['$scope', function ($scope) {
-    $currentViewScope = $scope;
-    currentView = sensorListView;
-    $.when(app.initialized).done(function () {
-      sensorListView.init(app, $scope);
-    });
-  }])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/dashboard', { controller: 'DashboardViewCtrl', templateUrl: dashboardView.templateUrl } )
-      .when('/sensors', { controller: 'SensorListViewCtrl', templateUrl: sensorListView.templateUrl } )
-      .otherwise({ redirectTo: '/dashboard' }); 
-  }])
-  .filter('moment', function () {
+  .controller ('SensorListViewCtrl', [
+    '$scope',
+    function ($scope) {
+      $currentViewScope = $scope;
+      currentView = sensorListView;
+      $.when (app.initialized).done (function () {
+        $scope.$evalAsync(function() {
+          sensorListView.init (app, $scope);
+        });
+      });
+    },
+  ])
+  .config ([
+    '$routeProvider',
+    function ($routeProvider) {
+      $routeProvider
+        .when ('/dashboard', {
+          controller: 'DashboardViewCtrl',
+          templateUrl: dashboardView.templateUrl,
+        })
+        .when ('/sensors', {
+          controller: 'SensorListViewCtrl',
+          templateUrl: sensorListView.templateUrl,
+        })
+        .otherwise ({redirectTo: '/dashboard'});
+    },
+  ])
+  .filter ('moment', function () {
     return function (datestr) {
-      return moment(datestr).format(MOMENT_FORMAT);
+      return moment (datestr).format (MOMENT_FORMAT);
     };
-  }); 
+  });
--- angular/scripts/upload_measurement.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sensorId = "One";
-const name = "MyIndustrialSensor";
-const token = "2TZNkmwiQcy8E5Jl";
-
-
